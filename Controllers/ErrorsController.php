c19d2f84d69a881d08172a43231773be129f603c
Anders Wesch <aw@jumpstory.com>
2022-05-01 10:20:20 -0700

    Fix facebook urls

--- Controllers/ErrorsController.php
+++ Controllers/ErrorsController.php
@@ -8,6 +8,24 @@ class ErrorsController extends Controller
      */
     public function error404()
     {
+        $url = $_SERVER["REQUEST_URI"];
+
+        // If from Facebook redirect to home
+        if (strpos($url, '?fbclid=') !== false) {
+            $urlPieces = explode('?', $url);
+            $originalUrl = substr($urlPieces[0], 1); // trim first char (/)
+
+            $this->redirectTo($originalUrl);
+        }
+
+        // If from Facebook add
+        if (strpos($url, '?fbadd') !== false) {
+            $urlPieces = explode('?', $url);
+            $originalUrl = substr($urlPieces[0], 1); // trim first char (/)
+
+            $this->redirectTo($originalUrl);
+        }
+
         echo "Error 404 Page Not Found";
     }
 }
